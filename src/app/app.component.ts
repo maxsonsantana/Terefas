50a5e88ded7b0ae6e22da4fdf429b5db0c0819ec
maxson.carvalho <maxson.carvalho@findes.org.br>
2020-06-07 10:57:49 -0300

    comitei novamente e dessa vez deu certogit status  =)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`p { font-weight: bold;}
+          div {color: gray; }`]
 })
 export class AppComponent {
-  title = 'Tarefas';
+  title = 'app';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+  itemCount = 4;
+  btnText = 'Adicionar uma tarefa';
+  objetivoText = 'Objetivos de vida';
+  objetivos = [];
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.itemCount = this.objetivos.length;
   }
-
+  addItem(){
+    this.objetivos.push(this.objetivoText);
+    this.objetivoText = '';
+    this.itemCount = this.objetivos.length;
+  }
 }
